e591a034802a608e964cc219a857a612064de0e7
Nidha-Balkees <nidhabalkees20@gmail.com>
2026-09-06 13:10:53 +0300

    Initial calculator project

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-const display = document.getElementById("display");
 
-function appendValue(value) {
-    display.value += value;
-}
+<script>
 
-function clearDisplay() {
-    display.value = "";
-}
+   
+    const display = document.getElementById("display");
 
-function deleteLast() {
-    display.value = display.value.slice(0, -1);
-}
 
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch {
-        display.value = "Error";
+    
+    function appendValue(value) {
+        display.value += value;
     }
-}
+
+
+    
+    function clearDisplay() {
+        display.value = "";
+    }
+
+
+    
+    function deleteLast() {
+        display.value = display.value.slice(0, -1);
+    }
+
+
+    
+    function calculate() {
+
+        try {
+            display.value = eval(display.value);
+        }
+
+        catch {
+            display.value = "Error";
+        }
+
+    }
+
+</script>
+
